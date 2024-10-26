c5fed970887c08daf598a75386af7b196dc498da
Ere15 <109438407+Ere15@users.noreply.github.com>
2024-10-26 23:34:16 +0300

    commit_testik

---
+++ hikiton/Assets/Scenes/Scene1-sleepingroom/Scripts/SoundManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance = null;
+    public AudioSource musicSource;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+}
